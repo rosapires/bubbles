f4dd93b5e3b7506fe2fd487dafcddf134db1b5f3
Rosa Pires <rosapires@desktop-2jku5fo.kea.local>
2018-05-14 12:01:53 +0200

    message2

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const divs = document.querySelectorAll("#app div");
+divs.forEach(function(div){
+//div.style.transform = `translate(50vw, 50vh)`;
+
+div.addEventListener("mouseenter", function(){
+    div.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh`;
+    div.style.backgroundColor = `hsl(${Math.random()*360},${Math.random()*100}%, 50%)`;
+
+});
+
+});
